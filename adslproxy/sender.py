eb29e9dba06d83891fce73e0da54a18ac3a2fa93
Metachen666 <mr.cl.mail.out@gmail.com>
2019-09-29 14:10:45 +0800

    修改

--- adslproxy/sender.py
+++ adslproxy/sender.py
@@ -37,7 +37,10 @@ class Sender():
         :return: 测试结果
         """
         try:
-            response = requests.get(TEST_URL, proxies={
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+            }
+            response = requests.get(TEST_URL, headers=headers, proxies={
                 'http': 'http://' + proxy,
                 'https': 'https://' + proxy
             }, timeout=TEST_TIMEOUT)
